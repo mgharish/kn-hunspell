8666fc0ec1c9170ed0fbedac6b1f49367878f1cb
mgharish <mgharish@gmail.com>
2012-10-17 19:40:25 +0530

    Modifying for installation

--- install.js
+++ install.js
@@ -1,9 +1,9 @@
-var err = initInstall("Kannada", "kn@dictionaries.addons.mozilla.org", "0.1");
+var err = initInstall("Kannada", "kn_IN@dictionaries.addons.mozilla.org", "2.0");
 if (err != SUCCESS)
     cancelInstall();
 
 var fProgram = getFolder("Program");
-err = addDirectory("", "kn@dictionaries.addons.mozilla.org",
+err = addDirectory("", "kn_IN@dictionaries.addons.mozilla.org",
 		   "dictionaries", fProgram, "dictionaries", true);
 if (err != SUCCESS)
     cancelInstall();

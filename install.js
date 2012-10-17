c4c486f414fb0faee32edd983b4f3eeadc7b045d
mgharish <mgharish@gmail.com>
2012-10-17 09:42:04 +0530

    Modified for installation

--- install.js
+++ install.js
@@ -1,9 +1,9 @@
-var err = initInstall("Kannada", "kn-hunspell@mgharish.addons.mozilla.org", "2.1");
+var err = initInstall("Kannada", "kn@dictionaries.addons.mozilla.org", "0.1");
 if (err != SUCCESS)
     cancelInstall();
 
 var fProgram = getFolder("Program");
-err = addDirectory("", "kn-hunspell@mgharish.addons.mozilla.org",
+err = addDirectory("", "kn@dictionaries.addons.mozilla.org",
 		   "dictionaries", fProgram, "dictionaries", true);
 if (err != SUCCESS)
     cancelInstall();

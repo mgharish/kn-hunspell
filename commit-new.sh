bd739f80ae368c479933cf24695ccb8597db9035
mgharish <mgharish@gmail.com>
2012-10-17 09:42:47 +0530

    Adding

--- commit-new.sh
+++ commit-new.sh
@@ -1,7 +1,7 @@
 export HUNSPELL_PATH="/usr/share/hunspell"
 
 git pull origin master
-sort -u $HUNSPELL_PATH/kn.dic | grep -v "^\s*$" > kn.dic
-sed "s#Released on.*\$#Released on `date`#"  $HUNSPELL_PATH/kn.aff > kn.aff
-git commit kn.aff kn.dic
+sort -u $HUNSPELL_PATH/kn.dic | grep -v "^\s*$" > dictionaries/kn.dic
+sed "s#Released on.*\$#Released on `date`#"  $HUNSPELL_PATH/kn.aff > dictionaries/kn.aff
+git commit *
 git push origin master

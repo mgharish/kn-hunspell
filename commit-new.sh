b7fd4ea05471a90ff2f7f8c7eb55d4b28868c753
mgharish <mgharish@gmail.com>
2012-10-18 00:31:59 +0530

    Correcting mistakes

--- commit-new.sh
+++ commit-new.sh
@@ -3,5 +3,5 @@ export HUNSPELL_PATH="/usr/share/hunspell"
 git pull origin master
 sort -u $HUNSPELL_PATH/kn.dic | grep -v "^\s*$" > dictionaries/kn.dic
 sed "s#Released on.*\$#Released on `date`#"  $HUNSPELL_PATH/kn.aff > dictionaries/kn.aff
-git commit dictionaries/* ./*
+git commit dictionaries/kn.dic dictionaries/kn.aff commit-new.sh install.rdf install.js
 git push origin master
